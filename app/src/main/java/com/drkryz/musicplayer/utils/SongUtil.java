950d9de119228195312ca04ffed98a5cea627e45
drpidman <augustocaio663@gmail.com>
2021-09-30 15:11:03 -0300

    update ui with new icons

--- app/src/main/java/com/drkryz/musicplayer/utils/SongUtil.java
+++ app/src/main/java/com/drkryz/musicplayer/utils/SongUtil.java
@@ -10,6 +10,14 @@ public class SongUtil {
     private String Duration;
     private String Album;
 
+    /**
+     * 
+     * @param title - music title
+     * @param author - music author/artist
+     * @param path - music path in storage
+     * @param duration - music string duration(convert to int)
+     * @param album - music album art
+     */
     public SongUtil(String title, String author, String path, String duration, String album) {
         Title = title;
         Author = author;
@@ -19,42 +27,82 @@ public class SongUtil {
     }
 
 
+    /**
+     * 
+     * @return {@link #Title}
+     */
     public String getTitle() {
         return Title;
     }
 
+    /**
+     * 
+     * @param title
+     */
     public void setTitle(String title) {
         Title = title;
     }
 
+    /**
+     * 
+     * @return {@link #Author}
+     */
     public String getAuthor() {
         return Author;
     }
 
+    /**
+     * 
+     * @param author
+     */
     public void setAuthor(String author) {
         Author = author;
     }
 
+    /**
+     * 
+     * @return {@link #Path}
+     */
     public String getPath() {
         return Path;
     }
 
+    /**
+     * 
+     * @param path
+     */
     public void setPath(String path) {
         Path = path;
     }
 
+    /**
+     * 
+     * @return {@link #Duration}
+     */
     public String getDuration() {
         return Duration;
     }
 
+    /**
+     * 
+     * @param duration
+     */
     public void setDuration(String duration) {
         Duration = duration;
     }
 
+    /**
+     * 
+     * @return {@link #Album}
+     */
     public String getAlbum() {
         return Album;
     }
 
+    /**
+     * 
+     * @param album
+     */
     public void setAlbum(String album) {
         Album = album;
     }
